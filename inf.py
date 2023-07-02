6c42cdeea1de729befe9306d13cc9c4b4e6ed528
Keshav Mishra <keshav.mishra0298@gmail.com>
2023-07-02 11:29:02 +0530

    add: test hugging face api

---
+++ inf.py
@@ -0,0 +1,13 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Classify 'Cardiac Pathways Corporation' into either of these classes ['Organisation', 'Institution', 'Government Body', 'Other']:\n",
+})
+print(output[0]['generated_text'].split("\n")[-1])
